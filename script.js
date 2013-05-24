b7e9e60da0158d8531e123448b3ae454fc8bb102
Sergey Troyanchuk <sergey.troyanchuk@gmail.com>
2013-05-24 20:30:59 +0300

    take input values, check digit field and put them into the object

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-$(document).ready(function() {
-	$('#carousel').html('test jQuery');
-	console.log('Testing console');
+$(document).ready(function(){
+	$("#addnew").click(function(){
+		title = $('#newtitle').val();
+		newval = $('#newval').val();
+		isdigit = checkinput('#newval');
+		
+		if(isdigit == true){
+			objtiket = new tiket(title,newval);
+			//console.log(objtiket);
+		}else{ 
+			$('#newval').val('Try again!');
+			alert('Please insert digit value inti the field!');
+		}
+	});
 });
+
+function checkinput(inputid)
+{
+		thisval = $(inputid).val();
+		isdigit = $.isNumeric(thisval);
+		return isdigit;
+}
+
+/* new object tiket */
+function tiket(title,startvalue)
+{
+	this.title = title;
+	this.stval = startvalue;
+}
